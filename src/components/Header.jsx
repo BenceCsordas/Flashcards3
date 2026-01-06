2005b74baedc2bcfb90effa18a5b182b5830756f
BenceCsordas <bencecs1@gmail.com>
2026-01-06 08:45:51 +0100

    finalfinal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { useNavigate } from 'react-router'
 import { FaHouseChimney } from "react-icons/fa6";
+import { MyAuthContext } from '../context/AuthContext';
+import { CgLogOut } from "react-icons/cg";
+import { useContext } from 'react';
 
 const Header = () => {
     const navigate = useNavigate()
+    const {hasAccess, clearKey} = useContext(MyAuthContext)
 
+    const handleLogout = () => {
+    clearKey()
+    navigate("/")
+    }
   return (
     <div className='header'>
         <span onClick={()=>navigate("/")}><FaHouseChimney size={25}/></span>
+        {<span onClick={handleLogout}><CgLogOut size={25}/></span>}
     </div>
   )
 }
